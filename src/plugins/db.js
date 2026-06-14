675fbaff69ade4336a23ea5bdc30205e97f2e474
thevjay <rebalvijay930@gmail.com>
2026-06-14 22:52:03 +0530

    Added codes

--- src/plugins/db.js
+++ src/plugins/db.js
@@ -1,22 +1,26 @@
 const fp = require('fastify-plugin');
 const mongoose = require('mongoose');
 
-async function dbConnector(fastify, options) {
+async function dbConnector(fastify) {
   try {
-    console.log("ENV:",process.env.MONGODB_URL)
-    await mongoose.connect(process.env.MONGODB_URL,{
+    console.log('ENV:', process.env.MONGODB_URL);
+
+    await mongoose.connect(process.env.MONGODB_URL, {
       serverSelectionTimeoutMS: 5000,
     });
+
     console.log('✅ MongoDB connected');
-    
+
     fastify.decorate('db', mongoose);
-    
+
     fastify.addHook('onClose', async () => {
       await mongoose.connection.close();
     });
+
   } catch (error) {
     console.error('MongoDB connection error:', error);
-    process.exit(1);
+
+    throw error; // <-- NOT process.exit(1)
   }
 }
 
